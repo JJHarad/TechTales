ef6354587be4b6d29aab4de0111b55aa35a92a59
JJHarad <jidnyaharad11.com>
2025-02-25 02:03:45 +0530

    last

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,34 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react(),
+//     require('@tailwindcss/forms'),
+
+//   ],
+//   build: {
+//     outDir: 'dist' // ✅ Ensure this is correct
+//   }
+// });
+// /*import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist' // ✅ Ensure this is correct
+//   }
+// });
+// */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -12,18 +43,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  build: {
-    outDir: 'dist' // ✅ Ensure this is correct
-  }
 });
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist' // ✅ Ensure this is correct
-  }
-});
-*/
- 
